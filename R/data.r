dbac64468e0b3f74c7019949f5d0e603f5926ea2
Kazumi Wada <kazwd2008@gmail.com>
2024-06-12 21:00:21 +0000

    version 0.1.1

---
+++ R/data.r
@@ -0,0 +1,24 @@
+#' Wine dataset in UCI Machine Learning Repository 
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#'
+#' @format ## `wine`
+#' A data frame with 178 rows and 13 columns:
+#' \describe{
+#'   \item{Alcohol}{Alcohol}
+#'   \item{Malic acid}{Malic acid}
+#'   \item{Ash}{Ash}
+#'   \item{Alcalinity of ash}{Alcalinity of ash}
+#'   \item{Magnesium}{Magnesium}
+#'   \item{Total phenols}{Total phenols}
+#'   \item{Flavonoids}{Flavonoids}
+#'   \item{Nonflavanoid phenols}{Nonflavanoid phenols}
+#'   \item{Proanthocyanins}{Proanthocyanins}
+#'   \item{Color intensity}{Color intensity}
+#'   \item{Hue}{Hue}
+#'   \item{OD280/OD315 of diluted wines}{OD280/OD315 of diluted wines}
+#'   \item{Proline}{Proline}
+#' }
+#' @source <https://archive.ics.uci.edu/dataset/109/wine>
+"wine"
